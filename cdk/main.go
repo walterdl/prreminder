477d01a496c202312761118f4bf3efcf75b8b920
Walter Devia <walter.devia@onevisionresources.com>
2024-06-16 09:56:25 -0500

    SQS for new messages in CDK stack.

--- cdk/main.go
+++ cdk/main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"os"
-
 	"github.com/aws/aws-cdk-go/awscdk/v2"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awsapigatewayv2"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awsapigatewayv2integrations"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awslambda"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awssqs"
 
 	"github.com/aws/constructs-go/constructs/v10"
 	"github.com/aws/jsii-runtime-go"
@@ -14,41 +13,6 @@ import (
 
 type AppStackProps struct {
 	awscdk.StackProps
-}
-
-func NewAppStack(scope constructs.Construct, id string, props *AppStackProps) awscdk.Stack {
-	var sprops awscdk.StackProps
-
-	if props != nil {
-		sprops = props.StackProps
-	}
-	stack := awscdk.NewStack(scope, &id, &sprops)
-
-	httpApi := awsapigatewayv2.NewHttpApi(stack, jsii.String("PRRemindHttpApi"), &awsapigatewayv2.HttpApiProps{
-		ApiName: jsii.String("PRRemindHttpApi"),
-	})
-
-	queueUrl := os.Getenv("NEW_MESSAGE_QUEUE_URL")
-	slackWebhookFn := awslambda.NewFunction(stack, jsii.String("SlackWebhook"), &awslambda.FunctionProps{
-		FunctionName: jsii.String("PRReminder-SlackWebhook"),
-		Code:         awslambda.Code_FromAsset(jsii.String("../slackwebhook/dist"), nil),
-		Runtime:      awslambda.Runtime_PROVIDED_AL2(),
-		Handler:      jsii.String("bootstrap"),
-		Architecture: awslambda.Architecture_ARM_64(),
-		Environment: &map[string]*string{
-			"NEW_MESSAGE_QUEUE_URL": &queueUrl,
-		},
-	})
-	slackWebhookIntegration := awsapigatewayv2integrations.NewHttpLambdaIntegration(jsii.String("slackWebhookHTTPIntegration"), slackWebhookFn, &awsapigatewayv2integrations.HttpLambdaIntegrationProps{})
-	httpApi.AddRoutes(&awsapigatewayv2.AddRoutesOptions{
-		Path: jsii.String("/slack"),
-		Methods: &[]awsapigatewayv2.HttpMethod{
-			"POST",
-		},
-		Integration: slackWebhookIntegration,
-	})
-
-	return stack
 }
 
 func main() {
@@ -65,29 +29,45 @@ func main() {
 	app.Synth(nil)
 }
 
-// env determines the AWS environment (account+region) in which our stack is to
-// be deployed. For more information see: https://docs.aws.amazon.com/cdk/latest/guide/environments.html
 func env() *awscdk.Environment {
-	// If unspecified, this stack will be "environment-agnostic".
-	// Account/Region-dependent features and context lookups will not work, but a
-	// single synthesized template can be deployed anywhere.
-	//---------------------------------------------------------------------------
 	return nil
+}
 
-	// Uncomment if you know exactly what account and region you want to deploy
-	// the stack to. This is the recommendation for production stacks.
-	//---------------------------------------------------------------------------
-	// return &awscdk.Environment{
-	//  Account: jsii.String("123456789012"),
-	//  Region:  jsii.String("us-east-1"),
-	// }
+func NewAppStack(scope constructs.Construct, id string, props *AppStackProps) awscdk.Stack {
+	var sprops awscdk.StackProps
 
-	// Uncomment to specialize this stack for the AWS Account and Region that are
-	// implied by the current CLI configuration. This is recommended for dev
-	// stacks.
-	//---------------------------------------------------------------------------
-	// return &awscdk.Environment{
-	//  Account: jsii.String(os.Getenv("CDK_DEFAULT_ACCOUNT")),
-	//  Region:  jsii.String(os.Getenv("CDK_DEFAULT_REGION")),
-	// }
+	if props != nil {
+		sprops = props.StackProps
+	}
+	stack := awscdk.NewStack(scope, &id, &sprops)
+
+	newSlackMessageQueue := awssqs.NewQueue(stack, jsii.String("PRRemind-NewSlackMessage"), &awssqs.QueueProps{
+		QueueName:         jsii.String("PRRemind-NewSlackMessage"),
+		VisibilityTimeout: awscdk.Duration_Seconds(jsii.Number(20)),
+	})
+
+	httpApi := awsapigatewayv2.NewHttpApi(stack, jsii.String("PRRemindHttpApi"), &awsapigatewayv2.HttpApiProps{
+		ApiName: jsii.String("PRRemindHttpApi"),
+	})
+
+	slackWebhookFn := awslambda.NewFunction(stack, jsii.String("SlackWebhook"), &awslambda.FunctionProps{
+		FunctionName: jsii.String("PRReminder-SlackWebhook"),
+		Code:         awslambda.Code_FromAsset(jsii.String("../slackwebhook/dist"), nil),
+		Runtime:      awslambda.Runtime_PROVIDED_AL2(),
+		Handler:      jsii.String("bootstrap"),
+		Architecture: awslambda.Architecture_ARM_64(),
+		Environment: &map[string]*string{
+			"NEW_MESSAGE_QUEUE_URL": newSlackMessageQueue.QueueUrl(),
+		},
+	})
+	slackWebhookIntegration := awsapigatewayv2integrations.NewHttpLambdaIntegration(jsii.String("slackWebhookHTTPIntegration"), slackWebhookFn, &awsapigatewayv2integrations.HttpLambdaIntegrationProps{})
+	httpApi.AddRoutes(&awsapigatewayv2.AddRoutesOptions{
+		Path: jsii.String("/slack"),
+		Methods: &[]awsapigatewayv2.HttpMethod{
+			"POST",
+		},
+		Integration: slackWebhookIntegration,
+	})
+
+	return stack
 }
